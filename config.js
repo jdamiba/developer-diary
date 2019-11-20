290a36705a4473569ad146ccff4ed5813efee67e
Joseph Damiba <joseph.damiba@plot.ly>
2019-11-19 20:05:03 -0500

    setting up gatsby project

---
+++ config.js
@@ -0,0 +1,68 @@
+"use strict"
+
+module.exports = {
+  url: "https://josephdamiba.com",
+  title: "Joseph Damiba's Developer Diary",
+  tagline: "A self-taught computer programmer with opinions.",
+  copyright: "© 2019 Joseph Damiba. All rights reserved.",
+  author: {
+    name: "Joseph Damiba",
+    bio: "A self-taught computer programmer with opinions.",
+    contacts: {
+      linkedin: "https://www.linkedin.com/in/joseph-damiba/",
+      github: "https://github.com/jdamiba",
+      twitter: "https://twitter.com/joedamiba",
+    },
+  },
+  labels: [
+    {
+      tag: "react",
+      tech: "React.js",
+      name: "DiReact",
+      size: 20,
+      color: "deepskyblue",
+    },
+    {
+      tag: "nodejs",
+      tech: "Node.js",
+      name: "DiNodejsSmall",
+      size: 20,
+      color: "lightgreen",
+    },
+    {
+      tag: "git",
+      tech: "Git",
+      name: "DiGitMerge",
+      size: 20,
+      color: "white",
+    },
+    {
+      tag: "javascript",
+      tech: "JavaScript",
+      name: "DiJsBadge",
+      size: 20,
+      color: "yellow",
+    },
+    {
+      tag: "css",
+      tech: "CSS",
+      name: "DiCss3Full",
+      size: 20,
+      color: "teal",
+    },
+    {
+      tag: "python",
+      tech: "Python",
+      name: "DiPython",
+      size: 20,
+      color: "deepskyblue",
+    },
+    {
+      tag: "html",
+      tech: "HTML",
+      name: "FaHtml5",
+      size: 20,
+      color: "darkorange",
+    }  
+  ],
+}
